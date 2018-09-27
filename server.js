e125e7d5ba172c17c0395507fe41338de12a89e1
jerbubias <43208559+jerbubias@users.noreply.github.com>
2018-09-27 15:15:42 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('This is the homepage for ORCWall!');
+});
+
+app.listen(port, () => {
+	console.log(`Server started on port ${port}`);
+});
