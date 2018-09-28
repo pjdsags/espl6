a78ca4b77ddf5baf299744f8b218d40ffa4ef514
jerbubias <jerbubias@users.noreply.github.com>
2018-09-28 12:58:06 +0100

    Updated Work

--- ORCWall/server.js
+++ ORCWall/server.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const path = require('path');
 
 const port = 3000;
 
+// Init App
 const app = express();
 
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Home Route
 app.get('/', (req, res) => {
-	res.send('This is the homepage for ORCWall!');
+	res.render('index', {
+		description: 'This is your feed'
+	});
+});
+
+// Add Route
+app.get('/profile', (req, res) => {
+	res.render('profile', {
+		description: 'This is your profile page'
+	});
 });
 
 app.listen(port, () => {
